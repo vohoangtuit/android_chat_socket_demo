b5e9d7a16ddc2961a237e7a199835fd346545102
vohoangtuit <vohoangtu2401@gmail.com>
2020-11-13 15:26:30 +0700

    commit code

---
+++ android/ChatSocket/app/src/main/java/vht/com/chatsocket/Constants.java
@@ -0,0 +1,7 @@
+package vht.com.chatsocket;
+
+public class Constants {
+    //public static final String CHAT_SERVER_URL = "https://socketio-chat.now.sh/";
+    public static final String CHAT_SERVER_URL = "http://192.168.29.61:3000/";//http://192.168.29.61:3000/
+    //public static final String CHAT_SERVER_URL = "https://chat2007.herokuapp.com/";
+}
